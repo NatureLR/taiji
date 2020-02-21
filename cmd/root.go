10e6e72f620b66e7ca4c1daf76535df2aedaa9a8
zxz <1127711564@qq.com>
2020-02-21 21:15:56 +0800

    去除根命令执行的动作

--- cmd/root.go
+++ cmd/root.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/NatureLingRan/go-project/app"
 	"github.com/NatureLingRan/go-project/version"
 	"github.com/spf13/cobra"
 )
@@ -15,12 +14,9 @@ var (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "go-project",
-	Short: "用创建go项目需要的文件",
-	Long:  `用于创建go项目的一些文件如Dockerfile,目录结构等`,
-	Run: func(cmd *cobra.Command, args []string) {
-		app.GoProject(projectName, projectPath)
-	},
+	Use:     "go-project",
+	Short:   "用创建go项目需要的文件",
+	Long:    `用于创建go项目的一些文件如Dockerfile,目录结构等`,
 	Version: version.Print(),
 }
 
