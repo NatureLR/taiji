d325df5397a70f36c54819da78ac315446636444
zxz <1127711564@qq.com>
2020-03-02 19:43:06 +0800

    Makefile 运行命令更新

--- pkg/template/dockerfile.go
+++ pkg/template/dockerfile.go
@@ -13,8 +13,7 @@ COPY .  .
 # 国内使用的goproxy
 #RUN export GOPROXY=https://goproxy.cn
 
-RUN go build -o {{.project}} -ldflags "-s -w" -mod=vendor .
-#RUN  CGO_ENABLED=0 GOOS=linux go build -o {{.project}} .
+RUN make build_in_docker
 
 # 运行
 
