2ce529df9bd3ad3cd4547d397bc5eb8c0c4fb17e
zxz <zxz@zxzdeMacBook-Pro.local>
2020-10-15 14:57:15 +0800

    makefile：增加help

--- pkg/template/Makefile.go
+++ pkg/template/Makefile.go
@@ -8,92 +8,69 @@ func init() {
 const Makefile = `
 # 判断当前commit是否有tag如果有tag则显示tag没有则显示 commit次数.哈希
 VER = $(shell echo "$(shell git log --oneline |wc -l).$(shell git log -n1 --pretty=format:%h)" | sed 's/ //g')
-
 # 用于判断当前的提前是否有tag
 TAG = $(shell git log -n1 --pretty=format:%h |git tag --contains)
-
 # 如果没有手动指定标签就使用自动生成的标签
 ifneq ($(TAG),)
-
-# 手动指定的标签
+# 通过git tag命令指定的标签
 VER = $(shell git tag --sort=committerdate |tail -1)
-
 endif
 
 GO_PATH = $(shell cat go.mod |grep module |cut -b 8-)
-
 VERSION =  -X '$(GO_PATH)/cmd.version=$(VER)'
-
 GO_VERSION = -X '$(GO_PATH)/cmd.goVersion=$$(go version | awk '{printf($$3)}')'
-
 GIT_COMMIT = -X '$(GO_PATH)/cmd.gitCommit=$$(git rev-parse HEAD)'
-
 BUILT = -X '$(GO_PATH)/cmd.built=$$(date "+%Y-%m-%d %H:%M:%S")'
-
 LDFLAG = "-s -w $(VERSION) $(GO_VERSION) $(GIT_COMMIT) $(BUILT)"
-
 PROJECT = {{.project}}
 
 # 二进制文件生成目录
 BIN_DIR = bin
-
 BUILD = go build -ldflags $(LDFLAG) -o $(BIN_DIR)/$(PROJECT) .
 
-# 编译为当前系统的二进制文件
-.PHONY: build
-build: 
+.PHONY: help
+help: ## 显示make的目标
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {sub("\\\\n",sprintf("\n%22c"," "), $$2);printf " \033[36m%-20s\033[0m  %s\n", $$1, $$2}' $(MAKEFILE_LIST)
 
+.PHONY: build
+build: ## 编译为当前系统的二进制文件
 	@$(BUILD)
 
-# 直接运行不编译
 .PHONY: run
-run:
-	
+run: ## 直接运行不编译
 	@go run .
 
-# 安装二进制文件到系统path
 .PHONY: install
-install:
-
+install: ## 安装二进制文件到系统path
 	@chmod +x $(BIN_DIR)/$(PROJECT) && mv $(BIN_DIR)/$(PROJECT) /usr/local/bin
 
-# 清理
 .PHONY: clean
-clean:
-	
+clean: ## 清理
 	@rm -rf bin	test	
 
-# 编译为docker镜像
 .PHONY: docker
 ifeq ($(REGISTRY),)
-docker:
+docker: ## 编译为docker镜像
 	@docker build -t $(PROJECT):latest -t $(PROJECT):$(VER) -f Dockerfile .
 else
-docker: 
+docker: ## 编译为docker镜像 
 	@docker build -t $(REGISTRY)/$(PROJECT):latest -t $(REGISTRY)/$(PROJECT):$(VER) -f Dockerfile .
 endif
 
 # Dockerfile中执行编译
 .PHONY: build_in_docker
 build_in_docker:
-
 	@CGO_ENABLED=0 GOOS=linux go build -ldflags $(LDFLAG) .
 
-# 交叉编译为windows的二进制文件
 .PHONY: windows
-windows:
-
+windows: ## 交叉编译为windows的二进制文件
 	@GOOS=windows $(BUILD)
 
-# 交叉编译为苹果osx的二进制文件
 .PHONY: darwin
-darwin:
-
+darwin: ## 交叉编译为苹果osx的二进制文件
 	@GOOS=darwin $(BUILD)
 
-# 交叉编译为arm的linux环境（树莓派等环境）二进制文件
 .PHONY: arm
-arm:
-
+arm: ## 交叉编译为arm的linux环境（树莓派等环境）二进制文件
 	@GOARCH=arm GOARM=7 GOOS=linux $(BUILD)
 `
