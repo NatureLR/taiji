6080d531fcfbdefecc77dcf876151612c18130ce
zxz <zxz@zxzdeMacBook-Pro.local>
2020-11-19 11:23:08 +0800

    makefile变量优化

--- pkg/template/build.go
+++ pkg/template/build.go
@@ -6,6 +6,8 @@ func init() {
 
 // BUILDMAKEFILE build目录下的makefile需要和根目录下的makefile合并
 const BUILDMAKEFILE = `
+project := {{.project}}
+
 .PHONY: help
 help: ## 显示make的目标
 	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {sub("\\\\n",sprintf("\n%22c"," "), $$2);printf " \033[36m%-20s\033[0m  %s\n", $$1, $$2}' $(MAKEFILE_LIST)
@@ -23,8 +25,8 @@ rpm:
 tgz: ## 将项目打包为tar.gz
 	mkdir -p tgz
 	docker run --rm -w / \
-		-v $(realpath $(CURDIR)/../../{{.project}}):/{{.project}} \
+		-v $(realpath $(CURDIR)/../../$(project)):/$(project) \
 		-v $(CURDIR)/tgz:/v \
 		alpine \
-		sh -c "tar -cf {{.project}}.tar.gz {{.project}} && mv {{.project}}.tar.gz /v"
+		sh -c "tar -cf $(project).tar.gz $(project) && mv $(project).tar.gz /v"
 `
